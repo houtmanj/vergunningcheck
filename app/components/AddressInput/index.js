e9d2c8ba3caaa174941c45feedfbdee003f438fd
Robin Piets <robinpiets@gmail.com>
2019-06-25 18:03:31 +0200

    Wrapped fields in <form> and added submit

--- app/components/AddressInput/index.js
+++ app/components/AddressInput/index.js
@@ -5,14 +5,28 @@ import { Button, TextField } from '@datapunt/asc-ui';
 import './style.scss';
 
 class AddressInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
   componentDidMount() {}
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log('submit');
+  }
 
   render() {
     return (
       <div className="address-input">
-        <TextField label="Postcode" />
-        <TextField label="Huisnummer" />
-        <Button>Submit</Button>
+        <form className="address-input-form" onSubmit={this.onFormSubmit}>
+          <TextField label="Postcode" />
+          <TextField label="Huisnummer" />
+          <Button>Submit</Button>
+        </form>
       </div>
     );
   }
