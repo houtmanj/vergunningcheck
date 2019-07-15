4e2271e30c87648f066e19a999c736b56edd210b
Robin Piets <robinpiets@gmail.com>
2019-07-15 16:40:01 +0200

    Updated AddressInputResult with PropTypes

--- app/components/AddressInputResult/index.js
+++ app/components/AddressInputResult/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AddressInputResult = ({ title, loading, loadingText = 'Laden...', children }) => (
+const AddressInputResult = ({ title, loading, loadingText, children }) => (
   <div>
     {title && <h4>{title}</h4>}
     {loading && <div>{loadingText}</div>}
     {children && !loading && <div>{children}</div>}
   </div>
 );
+
+AddressInputResult.defaultProps = {
+  loadingText: 'Laden...',
+};
+
+AddressInputResult.propTypes = {
+  title: PropTypes.string,
+  loading: PropTypes.bool,
+  loadingText: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
 
 export default AddressInputResult;
