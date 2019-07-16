9af4efc33285a186767764902040943ff59d410f
Robin Piets <robinpiets@gmail.com>
2019-07-16 13:30:03 +0200

    Cleaned AdressInput actions file

--- app/containers/AddressInput/actions.js
+++ app/containers/AddressInput/actions.js
@@ -1,9 +1,4 @@
-import {
-  FETCH_STREETNAME_REQUEST,
-  FETCH_BAG_REQUEST,
-  FETCH_MONUMENT_REQUEST,
-  FETCH_BESTEMMINGSPLAN_REQUEST,
-} from './constants';
+import { FETCH_STREETNAME_REQUEST, FETCH_BAG_REQUEST } from './constants';
 
 export const fetchStreetname = (query = '') => ({ type: FETCH_STREETNAME_REQUEST, query });
 
@@ -11,7 +6,3 @@ export const fetchBagData = (postcode, streetNumber) => ({
   type: FETCH_BAG_REQUEST,
   query: { postcode, streetNumber },
 });
-
-export const fetchMonumentData = (query = '') => ({ type: FETCH_MONUMENT_REQUEST, query });
-
-export const fetchBestemmingsplanData = (query = '') => ({ type: FETCH_BESTEMMINGSPLAN_REQUEST, query });
