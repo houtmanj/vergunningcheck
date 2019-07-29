4bef7a3864b05cc5af77d234cd7e6286e35d0b37
Robin Piets <robinpiets@gmail.com>
2019-07-29 15:13:35 +0200

    Removed translations

--- app/containers/AddressInput/messages.js
+++ app/containers/AddressInput/messages.js
@@ -5,20 +5,4 @@
  */
 import { defineMessages } from 'react-intl';
 
-export default defineMessages({
-  title: {
-    id: 'vergunningschecker.AddressInput.title',
-  },
-  postcode: {
-    id: 'vergunningschecker.AddressInput.postcode',
-  },
-  huisnummer: {
-    id: 'vergunningschecker.AddressInput.huisnummer',
-  },
-  submit: {
-    id: 'vergunningschecker.AddressInput.submit',
-  },
-  resultaat: {
-    id: 'vergunningschecker.AddressInput.resultaat',
-  },
-});
+export default defineMessages({});
