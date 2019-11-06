862b7a8fc1bcd9b291c0584c1e5a79a94aba49de
Robin Piets <robinpiets@gmail.com>
2019-11-06 13:44:11 +0100

    Disabled babelQuery

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -30,7 +30,7 @@ module.exports = options => ({
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: options.babelQuery,
+          // options: options.babelQuery,
         },
       },
       {
