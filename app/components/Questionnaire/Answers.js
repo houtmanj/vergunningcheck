c26ed0c10d7ef362f7ea68b0bf795151d66716a2
Robin Piets <robinpiets@gmail.com>
2019-11-19 13:28:23 +0100

    Refactored the Answers.js file

--- app/components/Questionnaire/Answers.js
+++ app/components/Questionnaire/Answers.js
@@ -7,63 +7,45 @@ import PrefilledAnswerText from './PrefilledAnswerText';
 
 import './style.scss';
 
-const StyledFooter = styled.div`
-  margin-top: 20px;
-`;
-
-const Answers = ({
-  className,
-  answers,
-  required,
-  userAnswers,
-  errors,
-  questionId,
-  onChange,
-  hideFooter,
-  hasRegistry,
-  setAnswer,
-}) => {
+const Answers = ({ className, answers, userAnswers, errors, questionId, onChange, hasRegistry, setAnswer }) => {
   const userAnswer = (userAnswers && userAnswers[questionId]) || null;
   const hasPrefilledAnswer = answers && answers.length ? answers.filter(answer => answer.prefilled).length > 0 : null;
 
   return (
     <>
-      <div>
-        {hasPrefilledAnswer && <PrefilledAnswerText />}
-        <div className={errors[questionId] ? 'error' : null}>
-          {errors[questionId] && <div className="error-label">{errors[questionId].message}</div>}
-          <div className={className}>
-            {answers &&
-              answers.map(answer => {
-                // // Set answer based on previous user input or from registry source
-                const checked =
-                  (userAnswer && userAnswer === answer.value) ||
-                  (hasRegistry && setAnswer && answer.value === 'true') ||
-                  (hasRegistry && !setAnswer && answer.value === 'false');
-                const answerId = `${questionId}-${answer.id}`;
+      {hasPrefilledAnswer && <PrefilledAnswerText />}
+      <div className={errors[questionId] ? 'error' : null}>
+        {errors[questionId] && <div className="error-label">{errors[questionId].message}</div>}
+        <div className={className}>
+          {answers &&
+            answers.map(answer => {
+              // Set answer based on previous user input or from registry source
+              const checked =
+                (userAnswer && userAnswer === answer.value) ||
+                (hasRegistry && setAnswer && answer.value === 'true') ||
+                (hasRegistry && !setAnswer && answer.value === 'false');
+              const answerId = `${questionId}-${answer.id}`;
 
-                return (
-                  <Label htmlFor={answerId} key={answerId} label={answer.optieText}>
-                    <input
-                      key={answer.id}
-                      type="radio"
-                      id={answerId}
-                      name={questionId}
-                      answer-id={answer.id}
-                      value={answer.value}
-                      data-id={answer.id}
-                      style={{ marginRight: 10, height: 30 }}
-                      onChange={e => onChange(e)}
-                      defaultChecked={checked}
-                      disabled="disabled"
-                    />
-                  </Label>
-                );
-              })}
-          </div>
+              return (
+                <Label htmlFor={answerId} key={answerId} label={answer.optieText}>
+                  <input
+                    key={answer.id}
+                    type="radio"
+                    id={answerId}
+                    name={questionId}
+                    answer-id={answer.id}
+                    value={answer.value}
+                    data-id={answer.id}
+                    style={{ marginRight: 10, height: 30 }}
+                    onChange={e => onChange(e)}
+                    defaultChecked={checked}
+                    disabled="disabled"
+                  />
+                </Label>
+              );
+            })}
         </div>
       </div>
-      {!hideFooter && required && <StyledFooter>* Vergunning nodig</StyledFooter>}
     </>
   );
 };
@@ -76,13 +58,11 @@ const StyledAnswers = styled(Answers)`
 
 Answers.propTypes = {
   className: PropTypes.string,
-  required: PropTypes.string,
   answers: PropTypes.array,
   errors: PropTypes.array,
   userAnswers: PropTypes.object,
   questionId: PropTypes.string,
   onChange: PropTypes.func,
-  hideFooter: PropTypes.bool,
   hasRegistry: PropTypes.bool,
   setAnswer: PropTypes.bool,
 };
