a515c9adafc21af618d43a1fc2f72dca9c24f318
Robin Piets <robinpiets@gmail.com>
2020-01-08 17:58:14 +0100

    Added Matomo via @datapunt motomo-tracker

--- app/app.js
+++ app/app.js
@@ -1,34 +1,29 @@
-/**
- * app.js
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-
-// Needed for redux-saga es6 generator support
 import '@babel/polyfill';
 
-// Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-
 import history from 'utils/history';
 import 'leaflet/dist/leaflet';
 
 // Import from @datapunt
 import { GlobalStyle as AmsterdamGlobalStyle, ThemeProvider, themeColor } from '@datapunt/asc-ui';
 import { createGlobalStyle } from '@datapunt/asc-core';
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 
-// Import root app
 import App from 'containers/App';
-
-// Load the favicon and the .htaccess file
+import { MATOMO_CONFIG } from './constants';
+import configureStore from './configureStore';
+// Load the favicon and the .htaccess fil
 import '!file-loader?name=[name].[ext]!./images/favicon.png';
 import 'file-loader?name=.htaccess!./.htaccess'; // eslint-disable-line import/extensions
 
-import configureStore from './configureStore';
+// Set Matomo tracker from @datapunt
+const instance = createInstance({
+  urlBase: MATOMO_CONFIG.BASE_URL,
+  siteId: MATOMO_CONFIG.SITE_ID,
+});
 
 // Create redux store with history
 const initialState = {};
@@ -43,15 +38,17 @@ const AppGlobalStyle = createGlobalStyle`
 `;
 
 ReactDOM.render(
-  <ThemeProvider>
-    <AmsterdamGlobalStyle />
-    <AppGlobalStyle />
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>
-  </ThemeProvider>,
+  <MatomoProvider value={instance}>
+    <ThemeProvider>
+      <AmsterdamGlobalStyle />
+      <AppGlobalStyle />
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </Provider>
+    </ThemeProvider>
+  </MatomoProvider>,
   MOUNT_NODE,
 );
 // Install ServiceWorker and AppCache in the end since
