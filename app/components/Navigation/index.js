6fc74149e7633db228d7fe9ec22968b40957d001
Robin Piets <robinpiets@gmail.com>
2020-01-09 10:54:49 +0100

    Added Matomo trackers in form button

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from '@datapunt/asc-core';
 import { Button } from '@datapunt/asc-ui';
 import { ChevronLeft } from '@datapunt/asc-assets';
+import { useMatomo } from '@datapunt/matomo-tracker-react';
 
 const NavigationStyle = styled(`div`)`
   display: flex;
@@ -14,33 +15,51 @@ const NavigationStyle = styled(`div`)`
   flex-direction: row-reverse;
 `;
 
-const Navigation = ({ showPrev, onGoToPrev, showNext, disableNext }) => (
-  <NavigationStyle>
-    <div>
-      {showNext && (
-        <Button type="submit" variant="secondary" disabled={disableNext} taskflow>
-          Volgende
-        </Button>
-      )}
-    </div>
-    <div>
-      {showPrev && (
-        <Button
-          variant="textButton"
-          iconLeft={<ChevronLeft />}
-          iconSize={14}
-          onClick={onGoToPrev}
-          style={{ marginLeft: 10 }}
+const Navigation = ({ page, showPrev, onGoToPrev, showNext, disableNext }) => {
+  const { trackEvent } = useMatomo();
+
+  const handleNextClick = () => {
+    trackEvent({ category: page, action: 'form-volgende-knop' });
+  };
+  const handlePrevClick = e => {
+    trackEvent({ category: page, action: 'form-vorige-knop' });
+
+    if (onGoToPrev) onGoToPrev(e);
+  };
+
+  return (
+    <NavigationStyle>
+      <div>
+        {showNext && (
+          <Button type="submit" variant="secondary" disabled={disableNext} onClick={handleNextClick} taskflow>
+            Volgende
+          </Button>
+        )}
+      </div>
+      <div>
+        {showPrev && (
+          <Button
+            variant="textButton"
+            iconLeft={<ChevronLeft />}
+            iconSize={14}
+            style={{ marginLeft: 10 }}
+            onClick={handlePrevClick}
             type="button"
-        >
-          Vorige
-        </Button>
-      )}
-    </div>
-  </NavigationStyle>
-);
+          >
+            Vorige
+          </Button>
+        )}
+      </div>
+    </NavigationStyle>
+  );
+};
+
+Navigation.defaultProps = {
+  page: 'undefined-page',
+};
 
 Navigation.propTypes = {
+  page: PropTypes.string,
   showPrev: PropTypes.bool,
   onGoToPrev: PropTypes.func,
   showNext: PropTypes.bool,
