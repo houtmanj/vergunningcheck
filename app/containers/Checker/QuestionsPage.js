91942375ad777c2a555439cae8088a45a7e6dcb8
Sven Jens <sven@sven-jens.nl>
2020-01-23 17:32:52 +0100

    Fix: Added useContext

--- app/containers/Checker/QuestionsPage.js
+++ app/containers/Checker/QuestionsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
 import history from 'utils/history';
