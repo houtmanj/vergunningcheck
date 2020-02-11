f89fb8584b18e23be2d30243097c34bd86255ca7
Sven Jens <sven@sven-jens.nl>
2020-02-11 19:34:51 +0100

    Fix: Readded link to olo

--- app/containers/Checker/ConclusionsPage.js
+++ app/containers/Checker/ConclusionsPage.js
@@ -5,21 +5,42 @@ import ReactMarkdown from 'react-markdown';
 import history from 'utils/history';
 import Form from 'components/Form/Form';
 import Navigation from 'components/Navigation';
-import { GET_CURRENT_TOPIC, PAGES } from '../../constants';
+import { EXTERNAL_URLS, GET_CURRENT_TOPIC, PAGES } from '../../constants';
 import { CheckerContext } from './CheckerContext';
 import DebugDecisionTable from '../../components/Questionnaire/DebugDecisionTable';
+import { connect } from 'react-redux';
 
 const uniqueFilter = (value, index, self) => self.indexOf(value) === index;
 
 const ConclusionsPage = () => {
   const { checker } = useContext(CheckerContext);
 
+  const goToOLO = e => {
+    e.preventDefault();
+    if (addressResults?.length === 1) {
+      // Form is validated, we can proceed
+
+      // Generate OLO parameter "postalCode"
+      const oloPostalCode = `facet_locatie_postcode=${addressResults[0].postcode}`;
+
+      // Generate OLO parameter "streetNumber"
+      const oloStreetNumber = `facet_locatie_huisnummer=${addressResults[0].huisnummer}`;
+
+      // Generate OLO parameter "suffix"
+      const oloSuffixValue = addressResults[0].toevoeging.replace(addressResults[0].huisnummer, '').trim();
+
+      const oloSuffix = `facet_locatie_huisnummertoevoeging=${oloSuffixValue}`;
+
+      // Redirect user to OLO with all parameters
+      window.open(
+        `${EXTERNAL_URLS.oloChecker.location}?param=postcodecheck&${oloPostalCode}&${oloStreetNumber}&${oloSuffix}`,
+        '_blank',
+      );
+    }
+  };
+
   return (
-    <Form
-      onSubmit={e => {
-        e.preventDefault();
-      }}
-    >
+    <Form onSubmit={goToOLO}>
       <Heading $as="h1">Conclusie</Heading>
 
       <Paragraph>Op basis van uw antwoorden ....</Paragraph>
@@ -52,11 +73,21 @@ const ConclusionsPage = () => {
         onGoToPrev={() => history.push(`/${GET_CURRENT_TOPIC()}/${PAGES.checkerResult}`)}
         showPrev
         showNext
-        nextText="Wat nu?"
+        nextText="Naar het omgevingsloket"
         formEnds
       />
       <DebugDecisionTable checker={checker} />
     </Form>
   );
 };
-export default ConclusionsPage;
+
+const mapStateToProps = state => {
+  const { addressResultsLoading, addressResults, bagLoading } = state.locationData;
+  return {
+    addressResultsLoading,
+    addressResults,
+    bagLoading,
+  };
+};
+
+export default connect(mapStateToProps)(ConclusionsPage);
