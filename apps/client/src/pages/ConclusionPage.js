8e814220736cf7b699c85f299dbd01963bfae010
Andre <afj.lambert@gmail.com>
2020-03-09 12:43:25 +0100

    Fix conclusion page

--- apps/client/src/pages/ConclusionPage.js
+++ apps/client/src/pages/ConclusionPage.js
@@ -11,8 +11,6 @@ import Form from "../components/Form";
 import Nav from "../components/Nav";
 import DebugDecisionTable from "../components/DebugDecisionTable";
 
-const uniqueFilter = (value, index, self) => self.indexOf(value) === index;
-
 const outcomes = {
   NEED_PERMIT: '"Vergunningplicht"',
   NEED_CONTACT: '"NeemContactOpMet"',
@@ -22,49 +20,34 @@ const ConclusionsPage = ({ topic, checker }) => {
   const history = useHistory();
   const { slug } = topic;
 
-  const needContact = !!checker.permits.find(
-    permit => permit.getOutputByDecisionId("dummy") === outcomes.NEED_CONTACT
-  );
-
-  const needPermit = !!checker.permits.find(
-    permit => permit.getOutputByDecisionId("dummy") === outcomes.NEED_PERMIT
-  );
-
   // find conclusions we want to display to the user
   // if outcome = contact then we return an array of len = 1
   // otherwise we return the descriptions for different permits
-  // const displayConclusions = checker.permits.reduce((acc, permit) => {
-  //   const dummyDecision = permit.getDecisionById("dummy");
-  //   const matchingRules = dummyDecision.getMatchingRules();
+  const displayConclusions = checker.permits.map(permit => {
+    const dummyDecision = permit.getDecisionById("dummy");
+    const matchingRules = dummyDecision.getMatchingRules();
+    const outcome = permit.getOutputByDecisionId("dummy");
 
-  //   if (permit.getOutputByDecisionId("dummy") === outcomes.NEED_CONTACT) {
-  //     return [matchingRules[0].description];
-  //   }
-  //   acc.push(matchingRules.map(rule => rule.description));
-  // }, []);
+    return {
+      outcome,
+      title:
+        outcome === outcomes.NEED_CONTACT
+          ? "Neem contact op met de gemeente"
+          : `${permit.name}: ${outcome}`,
+      description: matchingRules[0].description
+    };
+  });
 
-  // const iets = [{
-  //   permitName: 'Dakkapel bouwen',
-  //   title: 'Neem contact op met de gemeente'
-  //   conclusion: outcomes.NEED_CONTACT,
-  // },
-  // {
-  //   permitName: 'Dakkapel bouwen',
-  //   conclusion: outcomes.NEED_PERMIT,
-  //   conclusionText: 'U bent vergunningsplichtig.'
-  // }, {
-  //   permitName: 'Dakkapel monument',
-  //   conclusion: outcomes.PERMIT_FREE,
-  //   conclusionText: 'U bent vrij...'
-  // }]
-
-  // const needsContact = !!iets.find(({conclusion}) = conclusion === outcomes.NEED_CONTACT);
-  // const needsPermit = !!iets.find(({conclusion}) = conclusion === outcomes.NEED_PERMIT);
+  const contactPermit = displayConclusions.find(
+    ({ outcome }) => outcome === outcomes.NEED_CONTACT
+  );
+  const needsPermit = !!displayConclusions.find(
+    ({ outcome }) => outcome === outcomes.NEED_PERMIT
+  );
 
   const handleSubmit = e => {
     e.preventDefault();
-
-    if (needPermit) {
+    if (needsPermit) {
       window.open(OLO.home, "_blank");
     } else {
       history.push(geturl(routes.intro, { slug }) + "?resetChecker=true");
@@ -81,22 +64,22 @@ const ConclusionsPage = ({ topic, checker }) => {
           toepassing is.
         </Paragraph>
 
-        {/* {displayConclusions.map(conclusion => (
+        {(contactPermit || displayConclusions).map(({ title, description }) => (
           <>
-            <Heading $as="h2">{conclusion.title}</Heading>
+            <Heading $as="h2">{title}</Heading>
             <ReactMarkdown
-              source={conclusion.text}
+              source={description}
               renderers={{ paragraph: Paragraph }}
               linkTarget="_blank"
             />
-          </>      
-        )} */}
+          </>
+        ))}
 
         <Nav
           onGoToPrev={() => history.push(geturl(routes.results, { slug }))}
           showPrev
           showNext
-          nextText={needPermit ? "Naar het omgevingsloket" : "Opnieuw checken"}
+          nextText={needsPermit ? "Naar het omgevingsloket" : "Opnieuw checken"}
           formEnds
         />
         <DebugDecisionTable checker={checker} />
