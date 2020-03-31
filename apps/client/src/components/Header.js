7ac6cd4fd6547fc2a15259f57ba43f1e24713686
Robin Piets <robinpiets@gmail.com>
2020-03-31 17:58:24 +0200

    Feature: Updated Header according to design system

--- apps/client/src/components/Header.js
+++ apps/client/src/components/Header.js
@@ -1,47 +1,23 @@
 import React from "react";
-import { MenuItem, MenuButton } from "@datapunt/asc-ui";
-import { StyledHeader, StyledMenuInline } from "./HeaderStyles";
+import {
+  StyledHeader,
+  StyledHeaderWrapper,
+  StyledLogoWrapper,
+  StyledLogo,
+} from "./HeaderStyles";
 
-const MenuChildren = () => (
-  <>
-    <MenuItem>
-      <MenuButton
-      //   onClick={() =>
-      //     history.push(
-      //       `/${GET_CURRENT_TOPIC()}/${PAGES.locationIntroduction}`
-      //     )
-      //   }
-      // active={GET_CURRENT_PAGE() === PAGES.locationIntroduction}
-      >
-        Start
-      </MenuButton>
-    </MenuItem>
-    <MenuItem>
-      <MenuButton
-      //   onClick={() =>
-      //     history.push(`/${GET_CURRENT_TOPIC()}/${PAGES.location}`)
-      //   }
-      // active={GET_CURRENT_PAGE() === PAGES.location}
-      >
-        Locatie
-      </MenuButton>
-    </MenuItem>
-  </>
-);
 export const Header = ({ showLinks }) => (
   <StyledHeader
     tall
-    css={{ zIndex: 2 }}
-    backgroundColor="#fff"
+    css={StyledHeaderWrapper}
     homeLink={
       process.env.NODE_ENV === "production" ? "https://amsterdam.nl" : "/"
     }
-    title="Vergunningen"
-    navigation={
-      <>
-        <StyledMenuInline>{showLinks && <MenuChildren />}</StyledMenuInline>
-      </>
-    }
+    logo={() => (
+      <StyledLogoWrapper>
+        <StyledLogo />
+      </StyledLogoWrapper>
+    )}
   />
 );
 
